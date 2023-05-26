7f393c079cbd1f64151c918b22dc158e381d489c
sprite-enjoyer <samkharadze.saba01@gmail.com>
2023-05-26 20:10:10 +0400

    added cors allowed origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://fullstack-user-management-app-x9gk.vercel.app/"],
   methods: ["GET", "POST", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
